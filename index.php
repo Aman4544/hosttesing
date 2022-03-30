5ea8bd33719cbde330352b6079d5e3f5a9c22b0e
Aman4544 <95303000+Aman4544@users.noreply.github.com>
2022-03-30 14:53:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -44,11 +44,22 @@ $nama = $x["message"]["chat"]["first_name"];
 
 $text = $x["message"]["text"];
 
-if($text == "/start"){
-
-  $msg = "HELLO WELCOME OUR OUR BOT\nNAME -> $nama\nUSER ID -> $id \nUES /cmds TO VIEW MY COMMAND'S\n";
-}
-
+if($text == "/auth"){
+  $auth_key =substr($text, 5);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "$user_link");
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/15.0 Chrome/90.0.4430.210 Safari/537.36',
+  'authorization: '.$auth_key,
+  ));
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_COOKIEJAR, "cookie.txt");
+  curl_setopt($ch, CURLOPT_COOKIEFILE, "cookie.txt");
+  $result = curl_exec ($ch);
+  $msg = $result."\n";
+  }
 get($id,$msg);
 
 ?>
